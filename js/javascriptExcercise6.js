fdad721e754e1a775ea4e588d09c046b2fc1d41c
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-04-16 22:04:50 +0700

    update javascripteExercisep(6)

--- js/javascriptExcercise6.js
+++ js/javascriptExcercise6.js
@@ -38,36 +38,90 @@
 // })
 
 // 2.  
+// const fs = require("fs").promises;
+// function handleError(err) {
+//   if (err) {
+//     console.error("Error:", err);
+//   }
+// }
 
-const fs = require("fs");
-// handleError;
-const filePath = "text/example.txt";
-function handleError(err, data) {
+// const filePath = "text/example.txt";
+
+// function readFile(filePath) {
+//   return fs.readFile(filePath, "utf8");
+// }
+
+// function writeFile(filePath, data) {
+//   return fs.writeFile(filePath, data, "utf8");
+// }
+
+// const pathFileInput = "text/input.txt";
+
+// readFile(pathFileInput)
+//   .then((data) => {
+//     console.log("File contents:", data);
+//     const modifiedContent1 = data + "First modification";
+//     const pathFileOutput1 = "text/output1.txt";
+//     return writeFile(pathFileOutput1, modifiedContent1);
+//   })
+//   .then(() => {
+//     console.log("Successfully wrote to output1.txt");
+//     const pathFileOutput1 = "text/output1.txt";
+//     return readFile(pathFileOutput1);
+//   })
+//   .then((data) => {
+//     console.log("Content of output1.txt:", data);
+//     const modifiedContent2 = data + "Second modification";
+//     const pathFileOutput2 = "text/output2.txt";
+//     return writeFile(pathFileOutput2, modifiedContent2);
+//   })
+//   .then(() => {
+//     console.log("Successfully wrote to output2.txt");
+//     const pathFileOutput2 = "text/output2.txt";
+//     return readFile(pathFileOutput2);
+//   })
+//   .then((data) => {
+//     console.log("Final content of output2.txt:", data);
+//   })
+//   .catch((err) => {
+//     handleError(err);
+//   });
+
+
+  // 3.
+
+  const fs = require("fs").promises;
+
+async function handleError(err) {
   if (err) {
-    console.error("Error reading file:", err);
-  } else {
-    console.log("File contents:");
-    console.log(data);
+    console.error("Error:", err);
   }
 }
 
-const content = "hello every one";
-function writeFileAndPrint(filePath, modification, callBack) {
-  fs.writeFile(filePath, modification, "utf8", (err, data) =>
-    callBack(err, data)
-  );
+async function main() {
+  try {
+    const pathFileInput = "text/input.txt";
+    const data = await fs.readFile(pathFileInput, "utf8");
+    console.log("File contents:", data);
+
+    const modifiedContent1 = data + "First modification";
+    const pathFileOutput1 = "text/output1.txt";
+    await fs.writeFile(pathFileOutput1, modifiedContent1);
+    console.log("Successfully wrote to output1.txt");
+
+    const contentOutput1 = await fs.readFile(pathFileOutput1, "utf8");
+    console.log("Content of output1.txt:", contentOutput1);
+
+    const modifiedContent2 = contentOutput1 + "Second modification";
+    const pathFileOutput2 = "text/output2.txt";
+    await fs.writeFile(pathFileOutput2, modifiedContent2);
+    console.log("Successfully wrote to output2.txt");
+
+    const contentOutput2 = await fs.readFile(pathFileOutput2, "utf8");
+    console.log("Final content of output2.txt:", contentOutput2);
+  } catch (err) {
+    await handleError(err);
+  }
 }
-// writeFileAndPrint(filePath, content ,handleError);
 
-const writeFile = new Promise((resolve, reject)=>{
-    resolve(
-        
-    );
-})
-
-
-writeFile.then((result) => {
-    console.log(result);
-}).catch((err) => {
-    console.log(err)
-});
+main();
